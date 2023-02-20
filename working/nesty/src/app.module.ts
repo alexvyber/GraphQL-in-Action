10e988c7b25265540c0d8bb8aa6ead3fded092aa
Alexey Sokolov <alexvyber@gmail.com>
2023-02-20 23:02:44 +0300

    nesty setup

--- working/nesty/src/app.module.ts
+++ working/nesty/src/app.module.ts
@@ -1,10 +1,27 @@
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+// import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RecipesModule,
+    GraphQLModule.forRoot<YogaDriverConfig>({
+      driver: YogaDriver,
+      autoSchemaFile: 'schema.gql',
+      // transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
 })
 export class AppModule {}
